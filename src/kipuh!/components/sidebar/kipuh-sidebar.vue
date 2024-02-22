1d226c3cec4aa70be4eed89a62e56a4c0393e322
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2024-02-22 14:23:56 -0500

    feat

--- src/kipuh!/components/sidebar/kipuh-sidebar.vue
+++ src/kipuh!/components/sidebar/kipuh-sidebar.vue
@@ -1,25 +1,26 @@
-<template>
-  <div class="bg-yellow-400 h-full border-transparent">
-    <kipuh-option
-      v-bind="option.values"
-      v-for="(option, index) in options"
-      :key="index"
-    ></kipuh-option>
-  </div>
-</template>
-<script setup lang="ts">
-import { ref } from 'vue'
-import KipuhOption from './kipuh-option.vue'
-
-const options = ref([
-  {
-    values: {
-      optionName: 'Home',
-      optionIcon: 'pi pi-home',
-      subOptions: []
-    }
-  },
-  { values: { optionName: 'Products', optionIcon: 'bi bi-cart', subOptions: [{ suboptionName: 'New', suboptionIcon: 'bi bi-plus' }] } }
-])
-</script>
-<style scoped></style>
+    <template>
+      <div class="bg-yellow-400 h-full border-transparent">
+        <kipuh-option
+          v-bind="option.values"
+          v-for="(option, index) in options"
+          :key="index"
+        ></kipuh-option>
+      </div>
+    </template>
+    <script setup lang="ts">
+    import { ref } from 'vue'
+    import KipuhOption from './kipuh-option.vue'
+    
+    const options = ref([
+      {
+        values: {
+          optionName: 'Home',
+          optionIcon: 'pi pi-home',
+          subOptions: []
+        }
+      },
+      { values: { optionName: 'Products', optionIcon: 'bi bi-cart', subOptions: [{ suboptionName: 'New', suboptionIcon: 'bi bi-plus' }] } }
+    ])
+    </script>
+    <style scoped></style>
+    
